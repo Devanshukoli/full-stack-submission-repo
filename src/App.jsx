ffcf7f6f8b799ee7c8d8a0bd9182a0aa967455bd
Devanshukoli <kolidevanshu02@gmail.com>
2026-04-28 09:43:25 +0530

    add part component, refactor conent component

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,20 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+  return (
+    <>
+      <p>{props.part} { props.exercises}</p>
+    </>
+  )
+}
+
 const Content = (props) => {
   return (
     <>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
+      <Part part={ props.part1} exercises={props.exercises1} />
+      <Part part={ props.part2} exercises={props.exercises2} />
+      <Part part={ props.part3} exercises={props.exercises3} />
     </>
   )
 }
@@ -35,11 +43,9 @@ const App = () => {
     <div>
       <Header course={course} />
 
-      <Content
-        part1={part1} exercises1={exercises1}
-        part2={part2} exercises2={exercises2}
-        part3={part3} exercises3={exercises3}
-      />
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
 
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
