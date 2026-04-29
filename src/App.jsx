51fbf0663cb8bc5f9c7f7b96dabf361de38ed9fa
Devanshukoli <kolidevanshu02@gmail.com>
2026-04-29 08:44:10 +0530

    add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,35 @@
 const Header = (props) => {
   return (
-    <h1>{props.course}</h1>
-  )
-}
-
-const Part = (props) => {
-  return (
     <>
-      <p>{props.part} { props.exercises}</p>
-    </>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <>
-      <Part part={ props.part1} exercises={props.exercises1} />
-      <Part part={ props.part2} exercises={props.exercises2} />
-      <Part part={ props.part3} exercises={props.exercises3} />
-    </>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <h1>{props.course}</h1>
     </>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+
+  const part = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
 
   return (
     <div>
       <Header course={course} />
-
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
-
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
 }
 
-export default App
+export default App;
