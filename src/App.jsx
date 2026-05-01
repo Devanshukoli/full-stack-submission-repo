c9ae3b28ef883476a4328b9088536e1f202ef8af
Devanshukoli <kolidevanshu02@gmail.com>
2026-05-01 08:36:33 +0530

    add consoles to onclick attributes

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,13 @@ const App = () => {
         {counter}
       </div>
       {/* setting the counter inside onclick */}
-      <button onClick={() => setCounter(counter + 1)}>Plus</button>
+      <button onClick={() => { setCounter(counter + 1); console.log('increment') }}>Plus</button>
 
       {/* adding the reset button */}
-      <button onClick={() => setCounter(0)}>Reset</button>
+      <button onClick={() => { setCounter(0); console.log('reset') }}>Reset</button>
 
       {/** adding decrement counter */}
-      <button onClick={() => setCounter(counter - 1)}>Minus</button>
+      <button onClick={() => { setCounter(counter - 1);  console.log('decrement')}}>Minus</button>
     </>
   )
 }
