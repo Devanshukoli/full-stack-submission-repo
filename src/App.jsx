913ec5be9d61ef69aa8f7f6e20a97b995cdaad15
Devanshukoli <kolidevanshu02@gmail.com>
2026-05-01 08:26:19 +0530

    setting the counter inside onclick

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import { useState } from "react";
 const App = () => {
   const [counter, setCounter] = useState(0);
 
-  setTimeout(() => setCounter(counter + 1), 1000);
-
   return (
     <>
       <div>
         {counter}
       </div>
-      <button onClick={() => console.log('clicked')} onMouseEnter={() => console.log('mouse hovered.')}>Plus</button>
+      <button onClick={() => setCounter(counter + 1)}>Plus</button>
     </>
   )
 }
