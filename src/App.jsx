0f177af77a899efa704c90c94ae3ffdf59e9c3fb
Devanshukoli <kolidevanshu02@gmail.com>
2026-05-01 08:24:08 +0530

    add handleclick and mousehover events, functions in the onClick attribute

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,6 @@ import { useState } from "react";
 
 const App = () => {
   const [counter, setCounter] = useState(0);
-  const handleClick = () => {
-    console.log('clicked')
-  }
-
-  const mouseHover = () => {
-    console.log('mouse hover')
-  }
 
   setTimeout(() => setCounter(counter + 1), 1000);
 
@@ -17,7 +10,7 @@ const App = () => {
       <div>
         {counter}
       </div>
-      <button onClick={handleClick} onMouseEnter={mouseHover}>Plus</button>
+      <button onClick={() => console.log('clicked')} onMouseEnter={() => console.log('mouse hovered.')}>Plus</button>
     </>
   )
 }
