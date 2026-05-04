7dba85777152d99314fd8114d0c9e27e1c59cba2
Devanshukoli <kolidevanshu02@gmail.com>
2026-05-04 08:53:11 +0530

    add consoles to check re-render behavior

--- src/App.jsx
+++ src/App.jsx
@@ -18,16 +18,20 @@ const Button = (props) => {
 
 const App = () => {
   const [counter, setCounter] = useState(0);
-
+  console.log('rendering with counter value: ', counter)
+  
   const increaseCount = () => {
+    console.log('increasing, value before:', counter)
     return setCounter(counter + 1)
   }
-
+  
   const resetCount = () => {
+    console.log('reseting to zero, value before:', counter)
     return setCounter(0)
   }
-
+  
   const decrementCount = () => {
+    console.log('decreasing, value before:', counter)
     return setCounter(counter - 1)
   }
 
