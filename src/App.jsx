b928e54d448bbc83433277ac2946df037a2c2287
Devanshukoli <kolidevanshu02@gmail.com>
2026-05-05 08:24:05 +0530

    update app component, to learn avoid duplciation in state

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import { useState } from "react";
 
+const initialValues = [
+  { title: 'Pretzels', id: 0 },
+  { title: 'Crispy Seaweed', id: 1 },
+  { title: 'Granola', id: 2 },
+]
+
 const App = () => {
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setLastName] = useState('')
-  const fullName = firstName + ' ' + lastName;
-
-  const handleFirstNameChange = (e) => {
-    setFirstName(e.target.value)
-  }
-
-  const handleLastNameChange = (e) => {
-    setLastName(e.target.value)
-  }
+  const [items, setItems] = useState(initialValues);
+  const [selectedItem, setSelectedItems] = useState(items[0])
 
   return (
     <div>
-      <h1>Welcome to store!</h1>
-      <input value={firstName} onChange={handleFirstNameChange}></input>
-      <input value={lastName} onChange={handleLastNameChange}></input>
-
-      <p>So, You're fullname is <b>{fullName}</b></p>
+      <h1>What's in your travel pack?</h1>
+      <ul>
+        {items.map((i) => (
+          <li key={i.id}>
+            {i.title}
+            {' '}
+            <button onClick={() => setSelectedItems(i)}>button</button>
+          </li>
+        ))}
+      </ul>
+      <p>In my backpack I have : {selectedItem.title}</p>
     </div>
   )
 }
