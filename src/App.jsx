efa742157acb04ebf9670afd6e81440afef4f774
Devanshukoli <kolidevanshu02@gmail.com>
2026-05-07 08:41:18 +0530

    add history component to render conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
 import { useState } from 'react'
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
 export default function App() {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
@@ -26,7 +41,7 @@ export default function App() {
       <button onClick={handleRightClick}>right</button>
       {right}
 
-      <p>{allClicks.join(' ')}</p>
+      <History allClicks={ allClicks} />
       <p> Total : {total}</p>
     </div>
   )
