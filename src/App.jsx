597b02c285dad70f4196bd1cae616882f6d22909
Devanshukoli <kolidevanshu02@gmail.com>
2026-05-08 07:40:58 +0530

    passing function call in event handler

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,17 @@ export default function App() {
   const [value, setValue] = useState(10)
 
   const resetToZero = () => {
-    setValue(0)
+    const handler = () => {
+      console.log('reseting to zero')
+      setValue(0)
+    }
+    return handler;
   }
 
   return (
     <div>
       {value} 
-      <button onClick={() => resetToZero()}>reset to zero</button>
+      <button onClick={resetToZero()}>reset to zero</button>
     </div>
   )
 }
