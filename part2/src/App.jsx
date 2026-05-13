89b140400ceaccddfe79055e1d1cec16476887ec
Devanshukoli <kolidevanshu02@gmail.com>
2026-05-13 08:04:00 +0530

    update app component

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -4,6 +4,17 @@ import Note from '../components/Note'
 const App = (props) => {
 
   const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('new note...')
+
+  const addNote = (event) => {
+    event.preventDefault();
+    console.log('clicked event ', event.target)
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
 
   return (
     <div>
@@ -13,6 +24,13 @@ const App = (props) => {
           <Note key={note.id} note={note} />
         ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type='submit'>save</button>
+      </form>
     </div>
   )
 }
