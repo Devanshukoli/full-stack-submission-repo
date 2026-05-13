8764ff4456cd292be8c3ca64440a8b5edfdeb068
Devanshukoli <kolidevanshu02@gmail.com>
2026-05-13 07:57:05 +0530

    add note component, and update my main, app component as well.

--- part2/src/main.jsx
+++ part2/src/main.jsx
@@ -1,27 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
 const notes = [
   {
     id: 1,
     content: 'HTML is easy',
-    important: true
+    important: true,
   },
   {
     id: 2,
     content: 'Browser can execute only JavaScript',
-    important: false
+    important: false,
   },
   {
     id: 3,
     content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true
-  }
+    important: true,
+  },
 ]
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App notes={notes} />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App notes={notes} />
 )
