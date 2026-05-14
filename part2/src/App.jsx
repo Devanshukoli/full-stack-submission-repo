c2548030fa2f07c693768e02a83ee4c4d5bfc7bf
Devanshukoli <kolidevanshu02@gmail.com>
2026-05-14 07:39:00 +0530

    exercise 2.8: update phonebook component to have another number field

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,22 +1,27 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handlePhoneNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const addNumber = (event) => {
     event.preventDefault()
 
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     console.log(personObject.name)
+    console.log(personObject.number)
   
     const existingPerson = persons.find(
       (x) => x.name === personObject.name
@@ -37,6 +42,7 @@ const App = () => {
 
   }
 
+
   return (
     <div>
       <h2>Phonebook</h2>
@@ -45,12 +51,16 @@ const App = () => {
           name: <input onChange={handleNameChange} value={newName} />
         </div>
         <div>
+          phone-number: <input onChange={handlePhoneNumberChange} value={newNumber} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      {/* todo: add number as well. */}
       {persons.map((person) => (
-        <p key={person.id}>{person.name}</p>
+        <p key={person.id}>{person.name} { person.number}</p>
       ))}
     </div>
   )
