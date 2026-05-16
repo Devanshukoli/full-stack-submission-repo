121a0a4a9078ac787722512aab5f98f94b9e06b6
Devanshukoli <kolidevanshu02@gmail.com>
2026-05-16 07:42:10 +0530

    fetch data from the url and put that into initial state

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,19 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from '../components/Filter'
 import Performanceform from '../components/Personform'
 import Persons from '../components/Person'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
 
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchTerm, setNewSearchTerm] = useState('')
+
+  useEffect(() => {
+    fetch("http://localhost:3001/persons")
+      .then(res => res.json())
+      .then(res => {
+        setPersons(res)
+      })
+  }, [])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
