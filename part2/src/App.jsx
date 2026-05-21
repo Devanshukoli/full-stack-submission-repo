594751a6bf58385ed148bacf88433f0f8c16abad
Devanshukoli <kolidevanshu02@gmail.com>
2026-05-21 07:35:51 +0530

    add new functon to display the contruy details

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,23 +1,41 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+
+const DisplayCountryDetails = (props) => {
+  return (
+    <>
+      <h1>{props.name}</h1>
+      {props.languages.map(x => {
+        <li>{x}</li>
+      })}
+      <img src={props.flags.svg} alt={props.flags.alt} />
+      <span>{ props.postalCode}</span>
+      <span>{ props.currencies.name}</span>
+    </>
+  )
+}
+
 const App = () => {
   const [value, setValue] = useState('')
-  const [rates, setRates] = useState({})
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {
-    console.log('effect run, currency is now', country)
 
-    // skip if currency is not defined
+  useEffect(() => {
+    axios.get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+      .then(response => {
+        console.log(response.data)
+        // todo: where should I store this data, because I would like to use this existing data to show the feature working.
+      })
+  }, [])
+
+  useEffect(() => {
     if (country) {
-      console.log('fetching country information...')
       axios
         .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${country}`)
         .then(response => {
           console.log(response.data)
-          // setCountry(response.data)
-          // setRates(response.data.rates)
+          setCountry(response.data)
         })
     }
   }, [country])
@@ -34,11 +52,11 @@ const App = () => {
   return (
     <div>
       <form onSubmit={onSearch}>
-        currency: <input value={value} onChange={handleChange} />
-        <button type="submit">exchange rate</button>
+        Country: <input value={value} onChange={handleChange} />
+        <button type="submit">Show the info</button>
       </form>
       <pre>
-        {JSON.stringify(rates, null, 2)}
+        {JSON.stringify(country)}
       </pre>
     </div>
   )
