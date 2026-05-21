b9b69e876a5b56df43e06d3e492ecda455c6fa49
Devanshukoli <kolidevanshu02@gmail.com>
2026-05-21 07:58:36 +0530

    exercise 2.18: display the country

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,62 +1,73 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const CountryDetails = ({ country }) => {
+  const languages = country.languages
+    ? Object.values(country.languages).join(', ')
+    : ''
 
-const DisplayCountryDetails = ({ country }) => {
-  console.log('country.......', country)
   return (
-    <>
-      <h1>{country.name.common}</h1>
-      {country.languages.map(x => {
-        <li>{x}</li>
-      })}
-      <img src={country.flags.svg} alt={country.flags.alt} />
-      <span>{country.postalCode}</span>
-      <span>{country.currencies.name}</span>
-    </>
+    <div>
+      <h2>{country.name.common}</h2>
+      <p>capital {country.capital?.[0]}</p>
+      <p>area {country.area}</p>
+
+      <h3>languages</h3>
+      <p>{languages}</p>
+
+      <img src={country.flags.png} alt={country.flags.alt || `Flag of ${country.name.common}`} width="160" />
+    </div>
   )
 }
 
 const App = () => {
-  const [value, setValue] = useState('');
-  const [countries, setCountries] = useState([]);
+  const [value, setValue] = useState('')
+  const [countries, setCountries] = useState([])
   const [selectedCountry, setSelectedCountry] = useState(null)
-
 
   useEffect(() => {
     axios.get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
       .then(response => {
-        console.log(response.data)
         setCountries(response.data)
       })
   }, [])
 
-
-  const filteredCountry = value ? countries.filter((country) =>
-    country.name.common.toLowerCase()
-      .includes(value.toLowerCase())) : null;
-
- 
+  const filteredCountries = value
+    ? countries.filter(country =>
+      country.name.common.toLowerCase().includes(value.toLowerCase())
+    )
+    : []
 
   const handleChange = (event) => {
     setValue(event.target.value)
+    setSelectedCountry(null)
   }
 
-  const onSearch = (event) => {
-    event.preventDefault()
-    setCountries(value)
+  const showCountry = (country) => {
+    setSelectedCountry(country)
+    setValue(country.name.common)
   }
 
   return (
     <div>
-      <form onSubmit={onSearch}>
-        Country: <input value={value} onChange={handleChange} />
-        <button type="submit">Show the info</button>
-      </form>
-      <pre>
-        {JSON.stringify(countries)}
-      </pre>
-      <DisplayCountryDetails country={filteredCountry} />
+      <div>
+        find countries <input value={value} onChange={handleChange} />
+      </div>
+
+      {selectedCountry ? (
+        <CountryDetails country={selectedCountry} />
+      ) : filteredCountries.length > 10 ? (
+        <p>Too many matches, specify another filter</p>
+      ) : filteredCountries.length === 1 ? (
+        <CountryDetails country={filteredCountries[0]} />
+      ) : (
+        filteredCountries.map(country => (
+          <div key={country.cca3}>
+            {country.name.common}
+            <button onClick={() => showCountry(country)}>show</button>
+          </div>
+        ))
+      )}
     </div>
   )
 }
